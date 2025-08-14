5f55cd9b3ffb0d3c539733a3e7b7b130719423ba
Valerii Sysoiev <valsysoiev@gmail.com>
2025-08-14 17:29:43 -0600

    release: deploy v202508141729-620d1ce

--- app/api/schemas/__init__.py
+++ app/api/schemas/__init__.py
@@ -2,13 +2,6 @@
 from .summary import CountSummary, ActivityItem, EngagementSummary
 from .preset import PresetQuestion, PresetCapability, PresetPillar, AssessmentPreset
 
-# Import all other schemas from the main schemas.py file
-from ..schemas import (
-    AssessmentCreate, AssessmentResponse, AnswerUpsert, 
-    PillarScore, ScoreResponse, UploadResponse, UploadedFile,
-    EngagementCreate, EngagementResponse, EngagementUpdate
-)
-
 __all__ = [
     "CountSummary",
     "ActivityItem", 
@@ -16,15 +9,5 @@ __all__ = [
     "PresetQuestion",
     "PresetCapability",
     "PresetPillar",
-    "AssessmentPreset",
-    "AssessmentCreate",
-    "AssessmentResponse", 
-    "AnswerUpsert",
-    "PillarScore",
-    "ScoreResponse",
-    "UploadResponse",
-    "UploadedFile",
-    "EngagementCreate",
-    "EngagementResponse",
-    "EngagementUpdate"
+    "AssessmentPreset"
 ]
